4dab74757bfe800ad6224b25679ea7c6e0015745
Ben Gamari <bgamari.foss@gmail.com>
2014-03-11 15:20:38 -0400

    Move update-fab

--- update-fab.sh
+++ update-fab.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -e
 
 fab_outputs="gerbers front.png back.png"
+
+cd hardware
+rm -Rf $fab_outputs
+cd ..
+cd remote-board
+rm -Rf $fab_outputs
+cd ..
+
 git checkout fab
 git merge master
+
+cd hardware
 make $fab_outputs
 git add $fab_outputs
+cd ..
+
+cd remote-board
+make $fab_outputs
+git add $fab_outputs
+cd ..
+
 git commit -a -m "Update fab"
 git checkout master
